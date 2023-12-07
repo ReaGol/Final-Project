e020a4aa8ff7457abfc7acf19a68bfe19bb497d2
ReaGol <reutgoldin@gmail.com>
2023-12-07 18:11:25 +0200

    responsive for all screens

--- client/src/pages/homepage/Homepage.js
+++ client/src/pages/homepage/Homepage.js
@@ -3,30 +3,36 @@ import { Link } from "react-router-dom";
 import "../../App.css";
 import "./homepage.css";
 import logo from "../../images/PhysioTrack-logos_transparent.png";
-// import { FaChartLine, FaCheckSquare, FaEnvelope } from "react-icons/fa";
+import { FaChartLine, FaCheckSquare, FaEnvelope } from "react-icons/fa";
 
 function Homepage() {
   return (
     <div className='HomePage'>
-      <div className='image-bg'>
-        <div className='logo'>
-          <Link to='/'>
-            <img src={logo} alt='' />
-          </Link>
-        </div>
-        <h1 className='moto'>Healing Through Movement</h1>
-        <h3>Welcome physios!</h3>
-        <p className='short-p'>
-          This website was developed by and for physiotherapists, so that they
-          can create and upload an exercise plan for their patients quickly and
-          easily.
-        </p>
-        <p className="long-p">
-          No more sending your patients exercises on WhatsApp, no more wondering
-          whether they did them or not. From now on you can interact with your
-          patients and help them recover or achieve their goals faster.
-        </p>
+      <div className='logo'>
+        <Link to='/'>
+          <img src={logo} alt='' />
+        </Link>
       </div>
+      <h1 className='slogan'>Healing Through Movement</h1>
+
+      <div className='main-content'>
+        <div className='main-text'>
+          <h3>Welcome physios!</h3>
+          <p className='short-p'>
+            This website was developed by and for physiotherapists, so that they
+            can create and upload an exercise plan for their patients quickly
+            and easily.
+          </p>
+          <p className='long-p'>
+            No more sending your patients exercises on WhatsApp, no more
+            wondering whether they did them or not. From now on you can interact
+            with your patients and help them recover or achieve their goals
+            faster.
+          </p>
+        </div>
+        <div className='img-div'></div>
+      </div>
+
       <div className='register'>
         <button className='register-button'>Create Your Account</button>
         <p>
@@ -37,22 +43,22 @@ function Homepage() {
           </span>
         </p>
       </div>
-      {/* <section>
+      <div className='bottom-icons'>
         <div className='icons-div'>
-          <div className='card'>
+          <div className='icons-card'>
             <FaChartLine />
             <p>Track progress</p>
           </div>
-          <div className='card'>
+          <div className='icons-card'>
             <FaEnvelope />
             <p>Send reminders</p>
           </div>
-          <div className='card'>
+          <div className='icons-card'>
             <FaCheckSquare />
             <p>Upload Exercises</p>
           </div>
         </div>
-      </section> */}
+      </div>
       <footer className='copyright'>
         Photo by{" "}
         <a href='https://unsplash.com/@joshduke10?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash'>
