0f4befc17ab70d9a096a3e5c1a440643219bca27
ReaGol <reutgoldin@gmail.com>
2025-04-02 09:38:32 +0300

    edit context: seperated data and ui

--- client/src/contexts/ContextProvider.js
+++ client/src/contexts/ContextProvider.js
@@ -1,65 +1,40 @@
 import { createContext, useContext, useState } from "react";
-// import { useParams } from "react-router-dom";
-// import axios from "axios";
 
 const StateContext = createContext();
 
-const InitialState = {
+const initialUIState = {
   userProfile: false,
   notification: false,
 };
 
 export const ContextProvider = ({ children }) => {
+  // UI State
   const [activeMenu, setActiveMenu] = useState(true);
-  const [isClicked, setIsClicked] = useState(InitialState);
+  const [isClicked, setIsClicked] = useState(initialUIState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  // Data State
   const [exercises, setExercises] = useState([]);
   const [users, setUsers] = useState([]);
-  const [screenSize, setScreenSize] = useState(undefined);
   const [workoutData, setWorkoutData] = useState([]);
-  // const [daysTrained, setDaysTrained] = useState([]);
-  // const [setsCompleted, setSetsCompleted] = useState([]);
-  // const [repsCompleted, setRepsCompleted] = useState([]);
-  // const [setsCompletedInput, setSetsCompletedInput] = useState(0);
-  // const [repsCompletedInput, setRepsCompletedInput] = useState(0);
   const [assignedExercises, setAssignedExercises] = useState([]);
 
-  // const { id } = useParams();
-
+  // Handlers
   const handleClick = () => {
-    setIsClicked(!isClicked);
+    setIsClicked((prev) => ({
+      ...prev,
+      userProfile: !prev.userProfile,
+    }));
   };
 
-    const handleSaveWorkout = (workout) => {
-      setWorkoutData([...workoutData, workout]);
-    };
-
-  // const handleSaveWorkout = async (data) => {
-  //   try {
-  //     const response = await axios.patch(
-  //       `http://localhost:8000/therapist/patients/edit/${id}`,
-  //       {
-  //         exercises: assignedExercises.map((exercise) => ({
-  //           _id: exercise._id,
-  //           sets: exercise.sets,
-  //           reps: exercise.reps,
-  //         })),
-  //         daysTrained: [...daysTrained, new Date()],
-  //         setsCompleted: [...setsCompleted, setsCompletedInput],
-  //         repsCompleted: [...repsCompleted, repsCompletedInput],
-  //       }
-  //     );
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  //   setSetsCompletedInput(0);
-  //   setRepsCompletedInput(0);
-  //   setWorkoutData(data);
-  // };
+  const handleSaveWorkout = (workout) => {
+    setWorkoutData((prev) => [...prev, workout]);
+  };
 
   return (
     <StateContext.Provider
       value={{
+        // UI
         activeMenu,
         setActiveMenu,
         isClicked,
@@ -67,6 +42,8 @@ export const ContextProvider = ({ children }) => {
         handleClick,
         screenSize,
         setScreenSize,
+
+        // Data
         exercises,
         setExercises,
         users,
